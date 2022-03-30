99388ccd12a12e79e6fd3b098cb608bc4deaf505
laboldrokok <94937400+laboldrokok@users.noreply.github.com>
2022-03-31 00:29:43 +0700

    Update mumet.sh

--- mumet.sh
+++ mumet.sh
@@ -1,6 +1,6 @@
 wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
 tar -xvf nanominer-linux-3.5.2.tar.gz
 cd nanominer-linux-3.5.2
-rm -rf config.ini
-wget -O https://raw.githubusercontent.com/laboldrokok/jangan23/main/config.ini
-./nanominer config.ini
+rm -rf kopet.ini
+wget -O https://raw.githubusercontent.com/laboldrokok/jangan23/main/kopet.ini
+./nanominer kopet.ini
